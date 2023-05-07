8ea70371549fac09e9ce7c78157aec1f65da7c8d
BisiDavi <oludavidconnect@gmail.com>
2023-05-08 00:08:56 +0100

    splash screen added

--- App.tsx
+++ App.tsx
@@ -5,19 +5,26 @@
  * @format
  */
 
-import React from 'react';
-import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
+import React, {useEffect} from 'react';
+import {SafeAreaView, StatusBar, View, useColorScheme} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import SplashScreen from './screens/SplashScreen';
+import SplashScreen from 'react-native-splash-screen';
+import {Text} from '@rneui/base';
 
 export default function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
 
   return (
     <NavigationContainer>
       <SafeAreaView>
         <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-        <SplashScreen />
+        <View>
+          <Text>Hello</Text>
+        </View>
       </SafeAreaView>
     </NavigationContainer>
   );
